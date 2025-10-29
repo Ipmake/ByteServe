6a4d69a382432c61c009bb51c069c71e186bf840
Ipmake <niko.denis727@gmail.com>
2025-10-29 14:55:26 +0100

    fix: Fixed PurgeOldObjects task deleting existing objects

--- packages/backend/src/services/tasks/purge_old_objects.ts
+++ packages/backend/src/services/tasks/purge_old_objects.ts
@@ -1,30 +1,35 @@
 import { prisma } from "../..";
 import fs from "fs";
 import path from "path";
+import { getStorageDir } from "../../common/object-nesting";
 
 export default async function purgeOldObjects() {
-    const dbObjects = await prisma.object.findMany()
+    const dbObjects = await prisma.object.findMany({
+        include: {
+            bucket: true
+        }
+    })
 
     const objectsMap = new Map<string, string>();
     for (const obj of dbObjects) {
-        objectsMap.set(obj.id, obj.bucketId);
+        objectsMap.set(obj.id, obj.bucket.name);
     }
 
     let purgedCount = 0;
 
     // scan the storage directory find all objects within their buckets
-    const storageDir = path.join(__dirname, "../../../storage");
+    const storageDir = getStorageDir();
     const bucketDirs = await fs.promises.readdir(storageDir);
 
-    for (const bucketId of bucketDirs) {
-        const bucketPath = path.join(storageDir, bucketId);
+    for (const bucketName of bucketDirs) {
+        const bucketPath = path.join(storageDir, bucketName);
         const objectFiles = await fs.promises.readdir(bucketPath);
 
         for (const objectFile of objectFiles) {
             const objectId = path.parse(objectFile).name;
 
             // Check if the object exists in the database
-            if (!objectsMap.has(objectId) || objectsMap.get(objectId) !== bucketId) {
+            if (!objectsMap.has(objectId) || objectsMap.get(objectId) !== bucketName) {
                 // Object does not exist in DB, delete the file
                 const objectFilePath = path.join(bucketPath, objectFile);
                 await fs.promises.unlink(objectFilePath);
